1db328520f68bd8a6d593434f3950e9bf342f119
CriDos <CriDos@ya.ru>
2015-02-24 03:50:36 +0300

    Обновление версии.

--- info.h
+++ info.h
@@ -5,7 +5,7 @@
 //TODO: Не забывать инкрементировать версию;D
 namespace Info {
 static const int MAJOR = 1;
-static const int MINOR = 4;
+static const int MINOR = 5;
 
 static const char* PROGRAM_NAME = "DLLCollector_recode";
 }
